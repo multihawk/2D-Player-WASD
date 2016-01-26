7ba8e2157fd29b4a3a541813a3bf8c00a7971043
Hao Dinh <hqdinh@uci.edu>
2016-01-26 09:29:26 -0800

    Added 2D player WASD
    Description.

---
+++ 2D Player WASD/Assets/scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //moving left + right
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
+
+        //jumping
+
+        if (Input.GetKeyDown("space")){
+            transform.Translate(Vector2.up, Space.World);
+        }
+    }
+
+    
+}
